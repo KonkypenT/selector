fb446c9baa67719d1414792f622ad6f3ceb4e604
Dmitry <44831445+KonkypenT@users.noreply.github.com>
2019-03-05 09:50:33 +0700

    Update main.js
    Add property for change width on select.

--- main.js
+++ main.js
@@ -15,7 +15,41 @@ function fSelect() {
 function sSelect() {
     let department = document.getElementById('department');
     arrayStyle = {
+            'select': [{function fSelect() {
+    let department = document.getElementById('department');
+    let arrayDepartment = [{ 
+            'id': '1', 'title': 'IT' 
+        },
+        {
+            'id': '2', 'title': 'Frontend development'
+        },
+        { 
+            'id': '3', 'title': 'Backend development' 
+        }];
+    fillSelect(department, arrayDepartment);
+} fSelect();
+
+function sSelect() {
+    let department = document.getElementById('department');
+    arrayStyle = {
             'select': [{
+                'width': '300px',
+                'bGround': 'white',
+                'fColor': 'black',
+                'bColor': 'black',
+                'border': '1px solid rgba(0, 0, 0, 0.25)',
+                'bRadius': '5px',
+                'text': 'Please, change option'
+            }],
+            'option': [{
+                'bGround': '#f8f8f8',
+                'tBorder': '1px solid rgba(0, 0, 0, 0.25)',
+                'border': '1px solid rgba(0, 0, 0, 0.25)',
+                'bRadius': '5px'
+            }]
+        };
+    styleSelect(department, arrayStyle);
+} sSelect();
                 'bGround': 'white',
                 'fColor': 'black',
                 'bColor': 'black',
